c6eecfda6a8995e887d80771959cea3651f8d941
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2017-01-25 17:31:05 +0200

    live_view: fix the parse the group class for slider
    This patch fix live_view for range slider
    
    @fix
    
    Change-Id: I0708820ff191ff7688657e4ee97895ed4b0e5176

--- src/bin/ui/tabs.c
+++ src/bin/ui/tabs.c
@@ -818,6 +818,7 @@ _shortcut_tab_num_cb(void *data __UNUSED__,
    Tabs_Item *item;
 
    item = eina_list_nth(tabs.items, num - 1);
+   DBG("Switch tab by shortcut. Switch to tab #%d", num);
    if (item)
      elm_toolbar_item_selected_set(item->toolbar_item, true);
 }
